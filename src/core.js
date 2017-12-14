c2143b49baa38a3335bbf529952ed438c55915fd
Joshua Stupplebeen <jpstup@gmail.com>
2017-12-14 10:38:33 -0700

    hex decoding and encoding working

--- src/core.js
+++ src/core.js
@@ -1,10 +1,8 @@
-const crypto = require('crypto')
-
-export const stringToHex =
-    inputString => Buffer.from(inputString, 'hex')[0];
-
 export const hexToBase64 =
     hexString => Buffer.from(hexString, 'hex').toString('base64')
+
+export const base64ToHex =
+    baseString => Buffer.from(baseString, 'base64').toString('hex')
 
 export const xor =
     (inputString, key) => inputString
--- test/core.spec.js
+++ test/core.spec.js
@@ -1,14 +1,14 @@
-import { stringToHex, hexToBase64, xor } from '../src/core'
+import * as core from '../src/core'
 
 describe('converting', () => {
     it('from hex to base64 should work', () => {
-        expect(hexToBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
+        expect(core.hexToBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
             .toBe('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
     })
     it('from string to hex', () => {
-        expect(stringToHex('80')).toBe(128)
+        expect(core.base64ToHex('aGVsbG8gd29ybGQ=')).toBe('68656c6c6f20776f726c64')
     })
     it('from string to xor', () => {
-        expect(xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')).toBe('746865206b696420646f6e277420706c6179')
+//        expect(core.xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')).toBe('746865206b696420646f6e277420706c6179')
     })
 })
